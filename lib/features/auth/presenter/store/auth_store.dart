48a3d1a71d997798e6a001f2fb69db346db54b6e
Rodrigo Lima <rodrigolima9657@gmail.com>
2024-04-05 23:12:31 -0300

    add auth store

---
+++ lib/features/auth/presenter/store/auth_store.dart
@@ -0,0 +1,42 @@
+// ignore_for_file: library_private_types_in_public_api
+
+import 'package:mobx/mobx.dart';
+
+import '../../auth.dart';
+
+part 'auth_store.g.dart';
+
+class AuthStore = _AuthStoreBase with _$AuthStore;
+
+abstract class _AuthStoreBase with Store {
+  final SignUpUserUsecase signUpUserUsecase;
+
+  @observable
+  String? userId;
+
+  _AuthStoreBase({
+    required this.signUpUserUsecase,
+  });
+
+  @action
+  Future<void> signUpUser({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    final result = await signUpUserUsecase(
+      UserSignUpParams(name: name, email: email, password: password),
+    );
+
+    result.fold(
+      (failure) {
+        throw Error();
+      },
+      (userUid) {
+        userId = userUid;
+      },
+    );
+    // print(userId);
+    // print('userId');
+  }
+}
